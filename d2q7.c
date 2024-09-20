1d36df351461301b108bff536c42034cc217eec0
Nicolay Roness <nicroness@gmail.com>
2024-09-20 16:00:09 +0200

    add d2q7 collision phase

--- d2q7.c
+++ d2q7.c
@@ -1,9 +1,13 @@
 #include <assert.h>
+#include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <unistd.h>
+
+#define DIRECTIONS 7
+#define ALPHA 1
 
 typedef int64_t int_t;
 
@@ -13,9 +17,15 @@ typedef enum {
     FLUID
 } domain_t;
 
-int OFFSETS[2][6][2] = {
-    { {0,1}, {1,0}, {1,-1}, {0,-1}, {-1,-1}, {-1,0} }, /* Even rows */
-    { {0,1}, {1,1}, { 1,0}, {0,-1}, {-1, 0}, {-1,1} }  /* Odd rows */
+typedef struct {
+    double x, y;
+} vec2;
+
+double vec2_dot(vec2 a, vec2 b);
+
+int OFFSETS[2][7][2] = {
+    { {0, 0}, {0,1}, {1,0}, {1,-1}, {0,-1}, {-1,-1}, {-1,0} }, /* Even rows */
+    { {0, 0}, {0,1}, {1,1}, { 1,0}, {0,-1}, {-1, 0}, {-1,1} }  /* Odd rows */
 };
 
 void init_domain(void);
@@ -28,7 +38,10 @@ int_t N, M;
 int_t timesteps;
 char *input = NULL;
 
-domain_t *domain = NULL;
+domain_t *sites = NULL;
+double *densities[2] = { NULL, NULL };
+vec2 *v;
+vec2 e[6];
 
 int main(int argc, char **argv)
 {
@@ -44,12 +57,31 @@ int main(int argc, char **argv)
     printf("  Width  (N)    %lld\n", N);
     printf("  Height (M)    %lld\n", M);
 
-    for (int_t i = 0; i < timesteps; i++) {
-        collide();
-        stream();
+    densities[0] = malloc(DIRECTIONS * N * M * sizeof(double));
+    densities[1] = malloc(DIRECTIONS * N * M * sizeof(double));
+
+    for (int y = 0; y < M; y++) {
+        for (int x = 0; x < N; x++) {
+            for (int d = 0; d < DIRECTIONS; d++) {
+                densities[0][d * (y*N+x)] = 1.0 / 7.0;
+            }
+        }
     }
 
-    free(domain);
+    for( int_t d=0; d<6; d++ )
+    {
+        e[d].x = sin ( M_PI * d / 3.0 );
+        e[d].y = cos ( M_PI * d / 3.0 );
+    }
+
+    for (int_t i = 0; i < timesteps; i++) {
+        collide();
+        /*stream();*/
+    }
+
+    free(sites);
+    free(densities[0]);
+    free(densities[1]);
 
     return EXIT_SUCCESS;
 }
@@ -125,29 +157,29 @@ void init_domain()
     N = width;
     M = height;
 
-    domain = malloc(M * N * sizeof(uint8_t));
+    sites = malloc(M * N * sizeof(uint8_t));
     for (int_t y = 0; y < M; y++) {
         for (int_t x = 0; x < N; x++) {
             int16_t value = geometry[3*(y*width+x)] + geometry[3*(y*width+x)+1]
                 + geometry[3*(y*width+x)+2];
 
-            domain[y*width+x] = value > 0 ? FLUID : SOLID;
+            sites[y*width+x] = value > 0 ? FLUID : SOLID;
         }
     }
 
     // All SOLID points that are next to FLUID points are categorized as WALL
     for (int_t y = 0; y < M; y++ ) {
         for (int_t x = 0; x < N; x++) {
-            if (domain[y*width+x] != SOLID)
+            if (sites[y*width+x] != SOLID)
                 continue;
 
-            for (int_t i = 0; i < 6; i++) {
+            for (int_t i = 1; i < DIRECTIONS; i++) {
                 int_t nx = x + OFFSETS[y%2][i][0];
-                int_t ny = x + OFFSETS[y%2][i][1];
+                int_t ny = y + OFFSETS[y%2][i][1];
 
                 bool in_bounds = (nx >= 0 || nx < N || ny >= 0 || ny < M);
-                if (in_bounds && domain[ny*width+nx] == FLUID)
-                    domain[y*width+x] = WALL;
+                if (in_bounds && sites[ny*width+nx] == FLUID)
+                    sites[y*width+x] = WALL;
             }
         }
     }
@@ -158,24 +190,71 @@ void init_domain()
 
 void collide(void)
 {
-    FILE *out = fopen("out.ppm", "wr");
-    fprintf(out,"Colliding\n");
-    for (int_t y = 0; y < M; y++) {
-        for (int_t x = 0; x < N; x++) {
-            fprintf(out,"%d ", domain[y*N+x]);
+    int i           = 0;    // Lattice site
+    double rho      = 0.0;  // Density
+    double ev       = 0.0;  // Dot product of e and v;
+    double N_eq     = 0.0;  // Equilibrium
+
+    for (int y = 0; y < M; y++) {
+        for (int x = 0; x < N; x++) {
+            i = y*N+x;
+
+            // Ignore solid sites
+            if (sites[i] == SOLID)
+                continue;
+
+            rho = 0;
+            for (int d = 1; d < DIRECTIONS; d++) {
+                rho += densities[0][d*i];
+            }
+            ev = vec2_dot(e[i], v[i]);
+            for (int d = 0; d < DIRECTIONS; d++) {
+                // Boundary condition: Reflect of walls
+                if (sites[i] == WALL) {
+                    densities[1][(d%3)*i] = densities[0][d*i];
+                    continue;
+                }
+
+                N_eq = rho*(1.0-ALPHA)/6.0 + rho/3.0*ev + 2.0/3.0*rho*ev*ev - rho/6.0*v[i].x*v[i].y;
+                switch (sites[d]) {
+                    case FLUID:
+                        densities[1][d*i] = N_eq - densities[0][d*i];
+                        break;
+                    case WALL:
+                        densities[1][d*i] = N_eq - densities[0][d*i];
+                        break;
+                    case SOLID: break;
+                    default:
+                        assert(false && "Unknown domain type");
+                        break;
+                }
+            }
         }
-        fprintf(out,"\n");
     }
-    fclose(out);
 }
 
+
+/**
+ *  * • Collisions. For each lattice site, do the following:
+ * 1. From the nine microscopic densities ni
+ * , compute the macroscopic density
+ * ρ and velocity components ux and uy.
+ * 2. From these three macroscopic variables, use equation (8) to compute the
+ * equilibrium number densities n
+ * eq
+ * i
+ * .
+ * 3. Update each of the nine number densities according to equation (9).
+ * • Streaming. Move all the moving molecules into adjacent or diagonal lattice
+ * sites, by copying the appropriate ni values.
+*/
 void stream(void)
 {
     FILE *out = fopen("out.ppm", "wr");
     fprintf(out,"Streaming\n");
     for (int_t y = 0; y < M; y++) {
         for (int_t x = 0; x < N; x++) {
-            fprintf(out,"%d ", domain[y*N+x]);
+            fprintf(out,"%d ", sites[y*N+x]);
         }
         fprintf(out,"\n");
     }
